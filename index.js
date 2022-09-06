01f3bf80d6a22c2de36089519b9c21908236c6f4
Lam0509 <112493001+Lam0509@users.noreply.github.com>
2022-09-06 18:50:21 +0700

    second commit

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 function Calc() {
-    let a = parseInt(document.getElementById("amount").value);
-    let b = 23000 * a;
-    document.getElementById("result").innerHTML = 'Result: ' + b + ' VND'
+    let a = document.getElementById("opt1").value;
+    let b = document.getElementById("opt2").value;
+    let c = document.getElementById("amount").value;
+
+    if (a == b) {
+        document.getElementById("result").innerHTML = "Result: " + c + " " + a;
+    }
+
+    if (a == "VND" && b == "USD") {
+        document.getElementById("result").innerHTML = "Result: " + c / 23000 + " USD"
+    }
+
+    if (a == "USD" && b == "VND") {
+        document.getElementById("result").innerHTML = "Result: " + c * 23000 + " VND"
+    }
 }
+
+
